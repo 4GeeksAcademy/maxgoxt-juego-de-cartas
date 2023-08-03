a225fd39b80ec33316cfcae2648b1870bbbd29e0
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-08-03 18:14:51 +0000

    basic

--- src/app.js
+++ src/app.js
@@ -4,8 +4,43 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+// import { Generator } from "webpack";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("num").innerHTML = card();
+  document.getElementById("palo").innerHTML = palo();
+  let palo2 = document.getElementById("palo2");
+  palo2.innerHTML = document.getElementById("palo").innerHTML;
+};
+
+let card = () => {
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let numIdx = Math.floor(Math.random() * number.length);
+  return number[numIdx];
+};
+
+let palo = () => {
+  let palos = ["♦", "♥", "♠", "♣"];
+  let paloIdx = Math.floor(Math.random() * palos.length);
+  let p = document.getElementById("palo");
+  let p3 = document.getElementById("palo2");
+  if (palos[paloIdx] == "♦" || palos[paloIdx] == "♥") {
+    p.style.color = "red";
+    p3.style.color = "red";
+  }
+  return palos[paloIdx];
 };
